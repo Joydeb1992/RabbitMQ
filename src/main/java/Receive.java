1d8054640e7aa73d12da942297d3cef72b5f3e14
AyushMalewar <ayush.malewar@redbus.com>
2016-08-09 12:21:58 +0530

    great

--- src/main/java/Receive.java
+++ src/main/java/Receive.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 /**
  * Created by joydebbarman on 4/8/16. Branching done qwerty
+Working great!
  */
 public class Receive {
     private final static String QUEUE_NAME = "postoffice";
